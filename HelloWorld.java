3cdf7b57d410f01d284feec0867298e0bd4458e4
prapulubuntu <abc@gmail.com>
2022-03-23 11:06:17 +0000

    File commited

--- HelloWorld.java
+++ HelloWorld.java
@@ -5,6 +5,6 @@ class HelloWorld {
         System.out.println("Hello, World!"); 
         System.out.println("welcome to java"); 
         System.out.println("welcome to India"); 
-	System.out.println("welcome to India");
+	
     }
 }
